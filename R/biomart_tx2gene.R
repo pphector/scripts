4cb13f37f3ae9128663a84fe573ffc6c6644e8bc
Hector Galvez <jose.hector.galvez@computaitonalgenomics.ca>
2019-03-13 13:18:08 -0400

    Re-coded the tx2gene R script for added functionality and flexibility. Now follows Mugqic_tools conventions

--- R/biomart_tx2gene.R
+++ R/biomart_tx2gene.R
@@ -1,23 +1,77 @@
-## SETUP ## 
+# Downloads gene to transcript data from ENSEMBL
+# Produces an appropriately formated csv file for use with RNAseq light pipeline. 
+# Written by Hector Galvez
+# Usage: Rscript biomart_tx2gene.R --species Homo_sapiens --assembly GRCh38 --ensembl 87
 
-# Load libraries
-library("readr")
+### WARNING
+# NOTE: Only works on recent versions of ensembl that have the additional transcript_version available
+
+library("dplyr")
 library("biomaRt")
+library("methods")
+library("data.table")
 
-# Download transcript to gene conversion table (mus_musculus) from ENSEMBLE and format it
-## REQUIRES INTERNET CONNECTION
-# "Ensembl 90"     "Aug 2017" "http://Aug2017.archive.ensembl.org" 
+# Usage 
 
-mart <- biomaRt::useMart(biomart = "ENSEMBL_MART_ENSEMBL", dataset = "hsapiens_gene_ensembl", host = "dec2017.archive.ensembl.org")
-ttg <- biomaRt::getBM( attributes = c("ensembl_transcript_id", 
+usage=function(errM) {
+    cat("\nUsage: Rscript biomart_tx2gene.R [option] <Value>\n")
+    cat("           --species      : species (default= Homo_sapiens)")
+    cat("           --assembly     : assembly version (default= GRCh38)")
+    cat("           --ensembl      : ENSEMBL version (default= 87)")
+    cat("           --prefix       : output prefix path (defaults to current dir)")
+    stop(errM)
+}
+
+# Transcript to gene csv generating function
+write_tx2gene=function(ens.dataset, ens.host, outfile) {
+    mart <- biomaRt::useMart('ensembl', dataset = ens.dataset, host = ens.host)
+    ttg <- biomaRt::getBM( attributes = c("ensembl_transcript_id", 
                                       "transcript_version",
                                       "ensembl_gene_id",
                                       "external_gene_name",
                                       "description",
                                       "transcript_biotype"), mart = mart)  
-ttg <- dplyr::rename(ttg, target_id = ensembl_transcript_id,
+    ttg <- dplyr::rename(ttg, target_id = ensembl_transcript_id,
                        ens_gene = ensembl_gene_id, ext_gene = external_gene_name)
-ttg_id <- ttg
-ttg_id$target_id <- paste(ttg_id$target_id, ttg_id$transcript_version, sep = ".") 
+    ttg_id <- ttg
+    ttg_id$target_id <- paste(ttg_id$target_id, ttg_id$transcript_version, sep = ".") 
 
-write.csv(ttg_id, file="/home/hgalvez/projects/Elsehemy_RNAseq-NDP_PRJBFX-1652/hsapiens_ensembl90_tx2gene.csv", row.names = FALSE)
+    write.csv(ttg_id, file=outfile, row.names = FALSE)
+} 
+
+
+##################################
+
+ARG = commandArgs(trailingOnly = T)
+
+# default arg values
+species="Homo_sapiens"
+assembly="GRCh38"
+ens.vers="87"
+out.dir="."
+## get arg variables
+for (i in 1:length(ARG)) {
+    if (ARG[i] == "--species") {
+        species=ARG[i+1]
+    } else if (ARG[i] == "--assembly") {
+        assembly=ARG[i+1]
+    } else if (ARG[i] == "--ensembl") {
+        ens.vers=ARG[i+1]
+    } else if (ARG[i] == "--prefix") {
+        out.dir=ARG[i+1]
+    }
+} 
+
+source.name <- paste("Ensembl", ens.vers, sep="")
+csv.name <- paste(species, assembly, source.name, "tx2gene", "csv", sep = ".")
+
+outfile= paste(out.dir, csv.name, sep="/")
+
+
+EnsemblArchives <- listEnsemblArchives()
+host.url <- EnsemblArchives[EnsemblArchives$version == ens.vers,]$url
+full.mart <- biomaRt::useMart('ensembl', host=host.url)
+dataset.list <- listDatasets(full.mart)
+ens.dataset <- dataset.list[dataset.list$version %like% assembly,]$dataset
+
+write_tx2gene(ens.dataset, host.url, outfile)
