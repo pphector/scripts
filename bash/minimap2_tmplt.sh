b170332a63980a389ca5496770620cc5141fadec
Hector Galvez <jose.hector.galvez@computaitonalgenomics.ca>
2019-04-18 11:37:20 -0400

    Corrected some errors in the minimap script, and added more functionality

--- bash/minimap2_tmplt.sh
+++ bash/minimap2_tmplt.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
-#PBS -l walltime=48:00:00
-#PBS -l nodes=1:ppn=16
-#PBS -l mem=90G
+#PBS -l walltime=8:00:00
+#PBS -l nodes=1:ppn=8
+#PBS -l mem=32G
 #PBS -q sw
-#PBS -N PBjelly
+#PBS -N minimap2_REPLACE
 set -eu -o pipefail
 
 # Minimap2 alignment script. Useful for aligning assemblies to reference. 
@@ -13,7 +13,7 @@ SAMPLE="REPLACE"
 
 # Load minimap2
 module purge
-module load mugqic_dev/minimap2/2.16
+module load mugqic_dev/minimap2/2.16 mugqic/samtools/1.9
 
 # Define project directories
 PROJ="REPLACE"
@@ -23,12 +23,18 @@ ASSEMDIR="${PROJ}/REPLACE"
 ASSEM="${ASSEMDIR}/REPLACE"
 cd $ASSEMDIR
 mkdir minimap2_align
-OUTFILE="minimap2_align/REPLACE"
+OUTSAM="minimap2_align/REPLACE.sam"
+OUTBAM="minimap2_align/REPLACE.sorted.bam"
 
 # Reference mmi 
-REFASSEM="REPLACE"
+REFDIR="REPLACE"
+FASTAREF="${REFDIR}/REPLACE"
+MMIREF="${REFDIR}/REPLACE"
 PRESET="REPLACE" # Options include map-pb, map-ont, splice, sr, asm5, among others. See Docs. 
 
 # Alignment command, uses flag x to select a preset option
-# Preset 
-minimap2 -ax ${PRESET} ${REFASSEM} ${ASSEM} > ${OUTFILE}
+minimap2 -ax ${PRESET} ${MMIREF} ${ASSEM} > ${OUTSAM}
+
+# Sort and turn output to bam, then index. 
+samtools view -b ${OUTSAM} -@ 8 | samtools sort -@ 6 --reference ${FASTAREF} > ${OUTBAM} &&\
+samtools index ${OUTBAM}
