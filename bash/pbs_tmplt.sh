ff3b706c1e8668a31dfc7662bf3e5710f3871161
Hector Galvez <jose.hector.galvez@computaitonalgenomics.ca>
2019-06-19 16:33:08 -0400

    Added PBS template file

---
+++ bash/pbs_tmplt.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#PBS -l nodes=1:ppn=8
+#PBS -l mem=32G
+#PBS -l walltime=24:0:0
+#PBS -N REPLACE
+set -eu -o pipefail
+
+# Load Modules 
+module purge 
+
+# Define samples and names 
+
+
+# Other variables and Paths 
+
+
+# COMMAND 
+
+
