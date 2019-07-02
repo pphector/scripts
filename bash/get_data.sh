155fdf15a026c5983f370c710b91e4a350504fb4
Hector Galvez <jose.hector.galvez@computaitonalgenomics.ca>
2019-07-02 13:14:01 -0400

    modified get data script to use genpipes modules

--- bash/get_data.sh
+++ bash/get_data.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -eu -o pipefail
 echo "Starting import..."
-module load mugqic/python/2.7.8
+module load mugqic/python/2.7.8 mugqic/genpipes/3.1.4
 PID=${1}
 
-genpipes/utils/nanuq2mugqic_pipelines.py -p $PID -s HiSeq -a $HOME/.nanuqAuth
+nanuq2mugqic_pipelines.py -p $PID -s HiSeq -a $HOME/.nanuqAuth
 
 echo "Done"
